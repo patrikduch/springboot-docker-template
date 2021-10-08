00659f749055d13f31102db57adfb9cef56bd812
patrikduch <duchpatrik@icloud.com>
2021-10-08 20:13:41 +0200

    Initial commit

---
+++ src/test/java/com/patrikduch/springbootdockertemplate/SpringbootDockerTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrikduch.springbootdockertemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
